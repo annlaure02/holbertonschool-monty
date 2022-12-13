a0b1baa6629f81b1dfd9f59190b29c8adbc77f91
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-12-13 10:26:44 -0800

    get_op_funct added

---
+++ get_op_func.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * get_op_func - get function.
+ * Return: Nothing.
+ */
+void get_op_func(char *op, stack_t **stack, unsigned int line_number)
+{
+	unsigned int i = 0;
+
+	instruction_t code_list[] = {
+		{"push", _push},
+		{"pall", _pall},
+		//{"pint", _pint},
+		//{"pop", _pop},
+		//{"swap", _swap},
+		//{"add", _add},
+		//{"nop", _nop},
+		{NULL, NULL}
+	};
+
+	while (code_list[i].opcode != NULL)
+	{
+		//printf("code_list[i].opcode = %s\n", code_list[i].opcode);
+		if (strcmp(code_list[i].opcode, op) == 0)
+		{
+			code_list[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, op);
+	exit(EXIT_FAILURE);
+}
