3a2190cef7b74ab3c5b4f6d39d693f40a29106a4
annlaure02 <alaure.g02@gmail.com>
2022-12-14 07:46:44 -0800

    add error malloc failed on _push

--- op_func1.c
+++ op_func1.c
@@ -18,6 +18,7 @@ void _push(stack_t **stack, unsigned int line_number)
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
 	{
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new->n = n;
